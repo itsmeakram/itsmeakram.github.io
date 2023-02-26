65b3bb1a0f6c43c009b73c84a4d080ed1263b1a1
itsmeakram <103200000+itsmeakram@users.noreply.github.com>
2023-02-26 22:03:42 +0100

    Added new files

---
+++ akrame/html/template/default/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'You Site Name'"; //letter subject
+	$to = 'akramafkir@gmail.com'; // Enter Your E-mail
+	$from = 'akramafkir98@gmail.com'; // Admin e-mail
+	$fromName = 'akramprod'; // Your company name
+	$charset = 'UTF-8';
+?>
